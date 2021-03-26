e9111de0cb3dda35295f5509b74ae735183372a6
Dima <dmytrotarasov9@gmail.com>
2021-03-26 12:16:21 +0100

    created section-carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,36 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carouselArrow/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/carouselArrow/right.png"></button>',
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false,
-              }
-            },
-          ]
-      });
+// example of connecting Slick slider
+//$(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1000,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carouselArrow/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/carouselArrow/right.png"></button>',
+//         responsive: [
+//             {
+//               breakpoint: 992,
+//               settings: {
+//                 dots: true,
+//                 arrows: false,
+//               }
+//             },
+//           ]
+//       });
+//   });
+
+// example of connecting Tiny Slider
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false,
+  });
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+  });
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
   });
